f8467bbcb43582401082e045fd3a306f65d5891d
Tristan Eyma <teyma.ext@galerieslafayette.com>
2018-03-27 21:59:36 +0200

    ImpexBuilderControllerTests

--- src/test/java/com/galerieslafayette/pcm/impexbuilder/ecomimpexbuilder/controller/ImpexBuilderControllerTests.java
+++ src/test/java/com/galerieslafayette/pcm/impexbuilder/ecomimpexbuilder/controller/ImpexBuilderControllerTests.java
@@ -1,6 +1,9 @@
 package com.galerieslafayette.pcm.impexbuilder.ecomimpexbuilder.controller;
 
 import com.galerieslafayette.pcm.impexbuilder.ecomimpexbuilder.dao.AttributeValueRepository;
+import com.galerieslafayette.pcm.impexbuilder.ecomimpexbuilder.dto.ImpexBuilderDto;
+import com.galerieslafayette.pcm.impexbuilder.ecomimpexbuilder.exception.RecursionDepthException;
+import com.galerieslafayette.pcm.impexbuilder.ecomimpexbuilder.exception.WrongCategoryTypeException;
 import com.galerieslafayette.pcm.impexbuilder.ecomimpexbuilder.model.AttributeValue;
 import com.galerieslafayette.pcm.impexbuilder.ecomimpexbuilder.service.ImpexBuilderService;
 import org.assertj.core.api.Assertions;
@@ -10,7 +13,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 
 /**
  * @author teyma
@@ -26,9 +37,47 @@ public class ImpexBuilderControllerTests {
     @Mock
     private ImpexBuilderService impexBuilderService;
 
+    private final ImpexBuilderDto impexBuilderDto = Mockito.mock(ImpexBuilderDto.class);
+
     @Test
-    public void testBuildImpex() {
-        // TODO Unit tests
-        Assertions.assertThat(true).isFalse();
+    public void testBuildImpexShouldReturnNotFound() throws Exception {
+        testBuildImpexWithException(NoSuchElementException.class, HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void testBuildImpexShouldReturnBadRequest_FileNotFound() throws Exception{
+        testBuildImpexWithException(FileNotFoundException.class, HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void testBuildImpexShouldReturnBadRequest_WrongCategory() throws Exception{
+        testBuildImpexWithException(WrongCategoryTypeException.class, HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void testBuildImpexShouldReturnInternalServerError_IOException() throws Exception{
+        testBuildImpexWithException(IOException.class, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void testBuildImpexShouldReturnInternalServerError_RecursionDepth() throws Exception{
+        testBuildImpexWithException(RecursionDepthException.class, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void testBuildImpexOk() throws Exception{
+        ResponseEntity response = impexBuilderController.buildImpex(impexBuilderDto);
+        Mockito.verify(impexBuilderService).buildImpex(impexBuilderDto);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    private void testBuildImpexWithException(Class exceptionClass, HttpStatus expectedStatus) throws Exception{
+        Mockito.doThrow(exceptionClass).when(impexBuilderService).buildImpex(impexBuilderDto);
+
+        ResponseEntity response = impexBuilderController.buildImpex(impexBuilderDto);
+        Mockito.verify(impexBuilderService).buildImpex(impexBuilderDto);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(expectedStatus);
     }
 }
